4249cf73d8b16e05cffa3388b1c0cdcadab3b621
deluxesande <deluxesande@gmail.com>
2024-06-13 17:12:04 +0300

    (Fix): trying to load assets for android

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,9 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
+import { Asset } from "expo-asset";
+import { icons, images } from "../constants";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -11,6 +13,29 @@ export default function Layout() {
         DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
         DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
     });
+    const [isReady, setIsReady] = useState(false);
+    useEffect(() => {
+        async function loadAssets() {
+            try {
+                // Load the assets you want to use in your app
+                const imageAssets = Object.keys(images).map((key) =>
+                    Asset.loadAsync(images[key])
+                );
+                const iconAssets = Object.keys(icons).map((key) =>
+                    Asset.loadAsync(icons[key])
+                );
+
+                await Promise.all(imageAssets);
+                await Promise.all(iconAssets);
+
+                setIsReady(true);
+            } catch (error) {
+                console.error("Error loading assets:", error);
+            }
+        }
+
+        loadAssets();
+    }, []);
 
     const onLayoutRootView = useCallback(async () => {
         if (fontsLoaded) {
@@ -18,52 +43,8 @@ export default function Layout() {
         }
     }, [fontsLoaded]);
 
-    if (!fontsLoaded) {
+    if (!fontsLoaded || !isReady) {
         return null;
     }
     return <Stack onLayout={onLayoutRootView} />;
 }
-
-// import { useFonts } from "expo-font";
-// import { Stack } from "expo-router";
-// import { useCallback, useEffect, useState } from "react";
-// import * as SplashScreen from "expo-splash-screen";
-// import { Asset } from "expo-asset";
-
-// SplashScreen.preventAutoHideAsync();
-
-// export default function Layout() {
-//     const [fontsLoaded] = useFonts({
-//         DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-//         DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-//         DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
-//     });
-//     const [isReady, setIsReady] = useState(false);
-//     useEffect(() => {
-//         async function loadAssets() {
-//             try {
-//                 // Load the assets you want to use in your app
-//                 await Asset.loadAsync([
-//                     require("./assets/icon.png"),
-//                     require("./assets/image.jpg"),
-//                 ]);
-//                 setIsReady(true);
-//             } catch (error) {
-//                 console.error("Error loading assets:", error);
-//             }
-//         }
-
-//         loadAssets();
-//     }, []);
-
-//     const onLayoutRootView = useCallback(async () => {
-//         if (fontsLoaded) {
-//             await SplashScreen.hideAsync();
-//         }
-//     }, [fontsLoaded]);
-
-//     if (!fontsLoaded || !isReady) {
-//         return null;
-//     }
-//     return <Stack onLayout={onLayoutRootView} />;
-// }
